384fd72766df5d8b4e5f691b0d803549d475ac84
Smriti Srivastava <ssrivastava@tavisca.com>
2018-09-05 21:40:24 +0530

    shopping cart using SOLID principles

---
+++ ShoppingCartConsoleApp/ShoppingCartConsoleApp/Interfaces/ICartOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartConsoleApp.Interfaces
+{
+    interface ICartOperations
+    {
+        void AddItemToCart(Cart cart, Item item, int CountOfItemsToAdd);
+        void RemoveOneItemByIdFromCart(Cart cart, int itemId);
+        void RemoveAllItemsByIdFromCart(Cart cart, int itemId);
+        float GetTotalAmountOfCart(Cart cart);
+        void ClearCart(Cart cart);
+        List<Item> GetCartItems(Cart cart);
+    }
+}
